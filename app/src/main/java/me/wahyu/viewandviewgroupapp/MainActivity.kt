625741d2ee4f04c06e11e21c215dac935a9f3e82
Wahyu Akbar Wibowo <wahyuwibowoworker@gmail.com>
2022-02-13 19:13:53 +0700

    initial app

---
+++ app/src/main/java/me/wahyu/viewandviewgroupapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package me.wahyu.viewandviewgroupapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
